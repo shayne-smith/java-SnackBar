d0f0c5c193ce0d11b6b68d764bbeacaff8b5c0fc
seanmx96 <seanmx96@users.noreply.github.com>
2020-06-01 15:10:07 -0500

    Created Snack and VendingMachine classes

--- src/snackBar/Main.java
+++ src/snackBar/Main.java
@@ -2,7 +2,12 @@ package snackBar;
 
 public class Main {
 
-	public static void main(String[] args) {
+	private static void workWithData() {
+		System.out.println("Welcome to the Snack Bar!!!");
 		
+	}
+
+	public static void main(String[] args) {
+		workWithData();
 	}
 }
